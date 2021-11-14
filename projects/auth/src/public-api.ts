a29558f6bda02ac2ceecfac25c00b16a19588317
ashleyatdeloitte <85963825+ashleyatdeloitte@users.noreply.github.com>
2021-11-14 11:50:53 -0500

    Fixed production build issue
    Running `ng build auth` generated the following error:
    ------------------------------------------------------------------------------
    Building entry point 'auth'
    ------------------------------------------------------------------------------
    ✔ Compiling with Angular sources in Ivy full compilation mode.
    ✖ Generating FESM2020
    Invalid value for option "output.file" - when building multiple chunks, the "output.dir" option must be used, not "output.file". To inline dynamic imports, set t
    he "inlineDynamicImports" option.
    
    To resolve this error, you must import the submodules in public-api.ts

--- projects/auth/src/public-api.ts
+++ projects/auth/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/auth.service';
 export * from './lib/auth.component';
 export * from './lib/auth.module';
+export * from './lib/sign-in/sign-in.module';
+export * from './lib/sign-up/sign-up.module';
